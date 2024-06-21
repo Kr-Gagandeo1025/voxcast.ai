982ebbbaa4eb734552c195be0489ec34c987e0b5
Kr-Gagandeo1025 <kumargagandeo9@gmail.com>
2024-06-21 13:02:16 +0530

    did basic nav ui

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
-    <main className='min-h-screen'>
+    <main className='min-h-screen p-4'>
       <Nav/>
     </main>
   );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,17 @@
+'use client'
 import React from 'react'
+import longhamburger from '@/public/longhamburger.svg'
+import Image from 'next/image'
 
 const Nav = () => {
   return (
-    <div>
-      Navbar
+    <div className='flex justify-between items-center'>
+        <Image src={longhamburger} height={'auto'} width={150} alt='menu' className='w-[50px] md:w-[150px]'/>
+        <span className='text-4xl font-thin'>voxcast.ai</span>
+        <div className='border border-gray-300 rounded-full w-fit hidden md:flex gap-4'>
+            <button className='pl-4'>LOGIN</button>
+            <button className='h-full bg-green-400 rounded-full py-2 px-4'>SIGN UP &gt;</button>
+        </div>
     </div>
   )
 }
