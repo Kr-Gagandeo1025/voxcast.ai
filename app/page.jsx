264cd99739ee2a7f15df345bfff13f08f787906a
Prerna <omprernaroy@gmail.com>
2024-06-23 18:12:55 +0530

    added the logo

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className='min-h-screen p-4'>
       <Nav/>
+      <div className='flex flex-col justify-center items-center mt-12 pl-4'>
+      
+      </div>
     </main>
   );
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,19 +1,31 @@
 'use client'
-import React from 'react'
-import longhamburger from '@/public/longhamburger.svg'
-import Image from 'next/image'
+import React from 'react';
+import longhamburger from '@/public/longhamburger.svg';
+import voxcastailogo from '@/public/voxcastailogo.png';
+import Image from 'next/image';
 
 const Nav = () => {
   return (
     <div className='flex justify-between items-center'>
-        <Image src={longhamburger} height={'auto'} width={150} alt='menu' className='w-[50px] md:w-[150px]'/>
-        <span className='text-4xl font-thin'>voxcast.ai</span>
-        <div className='border border-gray-300 rounded-full w-fit hidden md:flex gap-4'>
-            <button className='pl-4'>LOGIN</button>
-            <button className='h-full bg-green-400 rounded-full py-2 px-4'>SIGN UP &gt;</button>
-        </div>
-    </div>
-  )
-}
+      {/* Hamburger (left) */}
+      <Image src={longhamburger} height={'auto'} width={150} alt='menu' className='w-[50px] md:w-[150px]' />
 
-export default Nav
+      {/* Logo */}
+      <div className='flex items-center'>
+        <Image src={voxcastailogo} height={50} width={50} className='h-11 mr-2' />
+        <span className='text-4xl font-thin'>voxcast.ai</span>
+        
+      </div>
+
+      {/* Login/Sign Up (right) */}
+      <div className='border border-gray-300 rounded-full w-fit hidden md:flex gap-4'>
+        <button className='pl-4'>LOGIN</button>
+        <button className='h-full bg-green-400 rounded-full py-2 px-4'>SIGN UP &gt;</button>
+        
+      </div>
+      </div>
+    
+  );
+};
+
+export default Nav;
