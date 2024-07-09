ce6385b919d35f7063ee63c8a2866076c7397535
Kr-Gagandeo1025 <kumargagandeo9@gmail.com>
2024-07-09 19:44:18 +0530

    newui ui fix test-final

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default function Page() {
   return(
-        <div className="h-screen w-screen flex flex-col items-center justify-start xl:px-12 py-2 px-4 bg-gradient-to-tr from-stone-100 via-transparent to-lime-200">
+        <div className="h-screen w-screen flex flex-col items-center justify-start p-4">
             <div className="w-full bg-lime-200 rounded-xl p-4">
               <span className="text-4xl font-bold">voxcast.ai</span>
             </div>
-          <div className="flex w-full h-full">
+          <div className="flex w-full h-full mt-2">
             <div className="p-12 md:flex flex-col bg-lime-200 rounded-2xl w-1/2 items-center justify-center hidden">
               <span className="text-5xl font-bold">Welcome to voxcast.ai</span>
               <span className="text-2xl mt-4">find the perfect podcast for any mood</span>
